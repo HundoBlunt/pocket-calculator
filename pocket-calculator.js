6969f2c0a3c2f720f55cc1f2a8cb0336c199366a
Tyler Nguyen <tylernguyen61903@gmail.com>
2019-01-15 10:41:25 -0500

    added things

--- pocket-calculator.js
+++ pocket-calculator.js
@@ -1,3 +1,73 @@
-function plus() {
-  
+var evalArr = ['','','']; var lastNum = true; var lastSym = false; var lastEql = false; currNum = 0; firstSym = false; var nonLoc = []; var numNums = 0;
+function insertNum(num){
+  if (numNums < 9) {
+    if (lastEql){
+      cleared(); lastEql = false; currNum = 0;
+    }
+    if (lastNum){
+      nonLoc.push(num);
+      let outNum = Number(nonLoc.join('')).toLocaleString();
+      document.getElementById('output').value = (outNum);
+      lastSym = true; numNums++;
+    } else {
+      nonLoc.push(num);
+      let outNum = Number(nonLoc.join('')).toLocaleString();
+      document.getElementById('output').value = (outNum);
+      lastSym = true; lastNum = true; numNums++;
+    }
+  evalArr[currNum] = evalArr[currNum] + num;
+  }
+}
+function parseDec(){
+  if (lastSym){
+    if (lastNum){
+      nonLoc.push('.');
+      document.getElementById('output').value = document.getElementById('output').value + '.';
+      evalArr[currNum] = evalArr[currNum] + '.';
+      lastSym = true; numNums++;
+    } else {
+      nonLoc.push('.');
+      document.getElementById('output').value = document.getElementById('output').value + '.';
+      evalArr[currNum] = evalArr[currNum] + '.';
+      lastSym = true; lastNum = true; numNums++;
+    }
+  }
+}
+function inserts(sym){
+  if (lastSym){
+    if (firstSym){
+      document.getElementById('output').value = (sym); nonLoc = []; numNums = 0;
+      lastSym = false; lastNum = false; lastEql = false; evalArr[1] = evalArr[1] + sym; currNum++; firstSym = true;
+    } else if(firstSym === false){
+      equals()
+      console.log(evalArr)
+      document.getElementById('output').value = (sym); nonLoc = []; numNums = 0;
+      lastSym = false; lastNum = false; lastEql = false; evalArr[1] = evalArr[1] + sym; currNum++; firstSym = false;
+    }
+  }
+}
+function equals(){
+  let evalStr = eval(evalArr.join(''))
+  if (evalStr > 999999999){
+    evalStr = evalStr.toExponential(9);
+  }
+  document.getElementById('output').value = evalStr.toLocaleString();
+  evalArr = ['','','']; evalArr[0] = evalArr[0] + eval(evalStr); currNum = 1; evalStr = ''; lastNum = false; lastEql = true;
+}
+function neg(){
+  if (lastSym){
+  document.getElementById('output').value = (document.getElementById('output').value * -1).toLocaleString();
+  console.log(currNum);
+  evalArr[currNum] = (evalArr[currNum] * -1);
+  }
+}
+function percentage(){
+  if (lastSym){
+    document.getElementById('output').value = (document.getElementById('output').value / 100);
+    evalArr[currNum] = (evalArr[currNum] / 100);
+  }
+}
+function cleared(){
+  document.getElementById('output').value = 0;
+  evalArr = ['','','']; lastNum = true; lastSym = false; currNum = 0; nonLoc = []; numNums = 0;
 }
