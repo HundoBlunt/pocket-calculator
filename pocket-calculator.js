7571b314432d975f94c185b6efe4759032cb08c1
Tyler Nguyen <tylernguyen61903@gmail.com>
2019-01-17 10:50:28 -0500

    added

--- pocket-calculator.js
+++ pocket-calculator.js
@@ -1,35 +1,43 @@
-var evalArr = ['','','']; var lastNum = true; var lastSym = false; var lastEql = false; currNum = 0; firstSym = false; var nonLoc = []; var numNums = 0;
+var evalArr = ['','','']; var lastNum = true; var lastSym = false; var lastEql = false; currNum = 0; firstSym = true; var nonLoc = []; var numNums = 0; var decimalExt = false; var afterDecArr = [];
 function insertNum(num){
-  if (numNums < 9) {
+  if (numNums < 9){
     if (lastEql){
-      cleared(); lastEql = false; currNum = 0;
+      clearOut(); lastEql = false; currNum = 0; evalArr = ['','','']; lastNum = true; lastSym = false; currNum = 0; nonLoc = []; numNums = 0; decimalExt = false; afterDecArr = [];
+
     }
     if (lastNum){
+      if (decimalExt){
+        let beforeDec = Number(nonLoc.join('')).toLocaleString();
+        afterDecArr.push(num);
+        let afterDec = afterDecArr.join('');
+        document.getElementById('output').value = (beforeDec+'.'+afterDec); lastSym = true; numNums++;
+      } else{
+        nonLoc.push(num);
+        let outNum = Number(nonLoc.join('')).toLocaleString();
+        document.getElementById('output').value = (outNum); lastSym = true; numNums++;
+      }
+    } else{
       nonLoc.push(num);
-      let outNum = Number(nonLoc.join('')).toLocaleString();
-      document.getElementById('output').value = (outNum);
-      lastSym = true; numNums++;
-    } else {
-      nonLoc.push(num);
-      let outNum = Number(nonLoc.join('')).toLocaleString();
-      document.getElementById('output').value = (outNum);
-      lastSym = true; lastNum = true; numNums++;
+      let outNum = nonLoc.join('').toLocaleString();
+      document.getElementById('output').value = (outNum); lastSym = true; lastNum = true; numNums++;
     }
   evalArr[currNum] = evalArr[currNum] + num;
   }
 }
 function parseDec(){
-  if (lastSym){
-    if (lastNum){
-      nonLoc.push('.');
-      document.getElementById('output').value = document.getElementById('output').value + '.';
-      evalArr[currNum] = evalArr[currNum] + '.';
-      lastSym = true; numNums++;
-    } else {
-      nonLoc.push('.');
-      document.getElementById('output').value = document.getElementById('output').value + '.';
-      evalArr[currNum] = evalArr[currNum] + '.';
-      lastSym = true; lastNum = true; numNums++;
+  if (numNums < 9){
+    if (decimalExt == false){
+      if (lastSym){
+        if (lastNum){
+          decimalExt = true;
+          document.getElementById('output').value = document.getElementById('output').value + '.';
+          evalArr[currNum] = evalArr[currNum] + '.'; lastSym = false; numNums++;
+        } else{
+          decimalExt = true;
+          document.getElementById('output').value = document.getElementById('output').value + '.';
+          evalArr[currNum] = evalArr[currNum] + '.'; lastSym = false; lastNum = true; numNums++;
+        }
+      }
     }
   }
 }
@@ -37,31 +45,34 @@ function inserts(sym){
   if (lastSym){
     if (firstSym){
       document.getElementById('output').value = (sym); nonLoc = []; numNums = 0;
-      lastSym = false; lastNum = false; lastEql = false; evalArr[1] = evalArr[1] + sym; currNum++; firstSym = true;
+      lastNum = false; lastEql = false; evalArr[1] = sym; currNum++;
     } else if(firstSym === false){
-      equals()
-      console.log(evalArr)
+      equals();
       document.getElementById('output').value = (sym); nonLoc = []; numNums = 0;
-      lastSym = false; lastNum = false; lastEql = false; evalArr[1] = evalArr[1] + sym; currNum++; firstSym = false;
+      lastNum = false; lastEql = false; evalArr[1] = sym; currNum++;
     }
   }
 }
 function equals(){
-  let evalStr = eval(evalArr.join(''))
+  if (eval(evalArr[0]) == 0 || eval(evalArr[2]) == 0){
+    if (evalArr[1] == '/'){
+      alert('Do not divide by zero!'); clearOut();
+    }
+  }
+  let evalStr = eval(evalArr.join(''));
   if (evalStr > 999999999){
     evalStr = evalStr.toExponential(9);
   }
   document.getElementById('output').value = evalStr.toLocaleString();
-  evalArr = ['','','']; evalArr[0] = evalArr[0] + eval(evalStr); currNum = 1; evalStr = ''; lastNum = false; lastEql = true;
+  evalArr = ['','','']; evalArr[0] = evalArr[0] + eval(evalStr); currNum = 1; evalStr = ''; lastNum = false; lastEql = true; firstSym = false;
 }
 function neg(){
   if (lastSym){
   document.getElementById('output').value = (document.getElementById('output').value * -1).toLocaleString();
-  console.log(currNum);
   evalArr[currNum] = (evalArr[currNum] * -1);
   }
 }
-function percentage(){
+function percent(){
   if (lastSym){
     document.getElementById('output').value = (document.getElementById('output').value / 100);
     evalArr[currNum] = (evalArr[currNum] / 100);
@@ -69,5 +80,5 @@ function percentage(){
 }
 function cleared(){
   document.getElementById('output').value = 0;
-  evalArr = ['','','']; lastNum = true; lastSym = false; currNum = 0; nonLoc = []; numNums = 0;
+  evalArr = ['','','']; lastNum = true; lastSym = false; currNum = 0; nonLoc = []; numNums = 0; decimalExt = false; afterDecArr = [];
 }
